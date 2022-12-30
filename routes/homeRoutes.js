c2dd83c9a62584fcfec7d7b0e6578bfeeefaff78
Japneet Singh <japneet8208@gmail.com>
2022-12-30 17:19:15 +0530

    Added sessions, venue,speakers pages

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -18,5 +18,41 @@ router.get('/speakers', (req, res, next) => {
         path: '/speakers'
     });
 })
+router.get('/venue', (req, res, next) => {
+    res.render('venue', {
+        pageTitle: 'Venue',
+        path: '/venue'
+    });
+})
+router.get('/sponsorship', (req, res, next) => {
+    res.render('sponsorship', {
+        pageTitle: 'Sponsorship',
+        path: '/sponsorship'
+    });
+})
+router.get('/callForPaper', (req, res, next) => {
+    res.render('callForPaper', {
+        pageTitle: 'Call For Paper',
+        path: '/callForPaper'
+    });
+})
+router.get('/paperSubmission', (req, res, next) => {
+    res.render('callForPaper', {
+        pageTitle: 'Paper Submission',
+        path: '/paperSubmission'
+    });
+})
+router.get('/sessions', (req, res, next) => {
+    res.render('sessions', {
+        pageTitle: 'Sessions',
+        path: '/sessions'
+    });
+})
+router.get('/publications', (req, res, next) => {
+    res.render('sessions', {
+        pageTitle: 'Publications',
+        path: '/publications'
+    });
+})
 
 exports.router = router;
